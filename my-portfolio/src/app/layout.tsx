5fc755ab11a70465f8420e9ec2fd4bb207489aac
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-01-26 10:46:49 -0600

    dark mode working on page and header. header has name and nav bar.

--- my-portfolio/src/app/layout.tsx
+++ my-portfolio/src/app/layout.tsx
@@ -1,5 +1,7 @@
+"use client"
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { ThemeProvider } from "next-themes";
 
 export default function RootLayout({
   children,
@@ -9,8 +11,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <Navbar/>
-        {children}
+        <ThemeProvider enableSystem={true} attribute="class">
+          <Navbar/>
+          {children}
+        </ThemeProvider>
         </body>
     </html>
   );
--- my-portfolio/src/components/Navbar.tsx
+++ my-portfolio/src/components/Navbar.tsx
@@ -7,11 +7,61 @@ import { useTheme } from "next-themes"
 import { RiMoonFill, RiSunLine } from "react-icons/ri"
 import { IoMdMenu, IoMdClose } from "react-icons/io"
 
+interface NavItem {
+    label: string
+    page: string
+}
+
+const NAV_ITEMS: Array<NavItem> = [
+    {
+        label: "Home",
+        page: "home"
+    },
+    {
+        label: "About",
+        page: "about"
+    },
+    {
+        label: "Projects",
+        page: "projects"
+    }
+]
+
 const Navbar = () => {
+    const {systemTheme, theme, setTheme} = useTheme()
+    const currentTheme = theme === "system" ? systemTheme : theme
+    const [navbar, setNavbar] = useState(false)
+
   return (
-    <div>
-      Navbar
-    </div>
+    <header className="w-full mx-auto px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
+      <div className="justify-between md:items-center md:flex">
+        <div>
+            <div className="md:py-5 md:black">
+                <h2 className="text-2xl font-bold">Cyril Malle-Barlow</h2>
+            </div>
+        </div>
+        <div className="md:flex md:space-x-6">
+            {NAV_ITEMS.map((item, i) => {
+                return <a key={i}>{item.label}</a>
+            })}
+            {currentTheme === "dark" ? (
+                <button 
+                    onClick={() => setTheme("light")}
+                    className="bg-slate-100 p-2 rounded-xl"
+                    >
+                        <RiSunLine size={25} color="black"/>
+                </button>
+            ) : (
+                <button 
+                onClick={() => setTheme("dark")}
+                className="bg-slate-100 p-2 rounded-xl"
+                >
+                    <RiMoonFill size={25}/>
+                </button>
+            )}
+        </div>
+      </div>
+    </header>
   )
 }
 
