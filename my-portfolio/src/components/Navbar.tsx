4b0caaf9dd06fc7daee863f0fa2b1b9bc33f2a47
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-01-25 18:26:34 -0600

    started project, installed dependencies, created navbar component

---
+++ my-portfolio/src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+"use client"
+import React from "react"
+import { useState } from "react"
+import { Link } from "react-scroll"
+import { usePathname } from "next/navigation"
+import { useTheme } from "next-themes"
+import { RiMoonFill, RiSunLine } from "react-icons/ri"
+import { IoMdMenu, IoMdClose } from "react-icons/io"
+
+const Navbar = () => {
+  return (
+    <div>
+      Navbar
+    </div>
+  )
+}
+
+export default Navbar
