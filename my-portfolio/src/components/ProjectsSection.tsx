68faccad74e0e5261992e5a1963d7ddbee1ef729
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-02-02 10:33:12 -0600

    added two projects, ill have to had more lateer.... when i have them...

--- my-portfolio/src/components/ProjectsSection.tsx
+++ my-portfolio/src/components/ProjectsSection.tsx
@@ -12,6 +12,14 @@ const projects = [
     github: 'https://github.com/cyrilmb/Haiku-For-You',
     link: 'coming soon',
   },
+  {
+    name: 'Baby Know',
+    description:
+      'Baby Know is a learning management system (LMS) that hosts content for the Baby Know: Bodies, Hearts & Minds program, which provides new parents with a holistic approach to their child’s development. The application is built for three types of users: administrators, students and teachers. The Baby Know program includes information, tools and strategies to create strong parent-child relationships, incorporating developmental milestones and nurturing the emotional well-being of all family members. Parents learn through video lectures, demonstrations, and activities, covering all aspects of development.',
+    image: '/baby-know.png',
+    github: 'https://github.com/cyrilmb/baby-know',
+    link: 'coming soon',
+  },
 ];
 
 const ProjectsSection = () => {
