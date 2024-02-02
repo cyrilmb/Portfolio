bfc0c23c93f56f7a7b2d51699de368c4f3384393
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-02-02 10:49:06 -0600

    styled projects section, need to update links

--- my-portfolio/src/components/ProjectsSection.tsx
+++ my-portfolio/src/components/ProjectsSection.tsx
@@ -23,7 +23,53 @@ const projects = [
 ];
 
 const ProjectsSection = () => {
-  return <div></div>;
+  return (
+    <section id="projects">
+      <h1 className="text-center font-bold text-4xl">
+        Projects
+        <hr className="w-9 h-1 mx-auto my-4 bg-blue-600 border-0 rounded" />
+      </h1>
+      <div className="flex flex-col space-y-28">
+        {projects.map((project, i) => {
+          return (
+            <div key={i}>
+              <div className="flex flex-col md:flex-row md:space-x-12">
+                <div className="md:w-1/2">
+                  <Image
+                    src={project.image}
+                    alt={`Screenshot of the app ${project.name}`}
+                    width={1000}
+                    height={1000}
+                    className="rounded-xl shadow-xl hover:opacity-70"
+                  />
+                </div>
+                <div className="md:w-1/2">
+                  <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
+                  <p className="text-xl leading-7 text-neutral-600 dark:text-neutral-400">
+                    {project.description}
+                  </p>
+                  <div className="flex flex-row align-bottom space-x-4">
+                    <Link href={project.github} target="_blank">
+                      <BsGithub
+                        size={30}
+                        className="hover:-translate-y-1 transition-transform cursor-pointer"
+                      />
+                    </Link>
+                    <Link href={project.link} target="_blank">
+                      <BsArrowUpRightSquare
+                        size={30}
+                        className="hover:-translate-y-1 transition-transform cursor-pointer"
+                      />
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default ProjectsSection;
