62674fdcc01c03409cac089c8708a9577146bb9a
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-02-05 21:03:36 -0600

    need to rewrite some project descriptions and image formatting

--- my-portfolio/src/components/ProjectsSection.tsx
+++ my-portfolio/src/components/ProjectsSection.tsx
@@ -7,7 +7,7 @@ const projects = [
   {
     name: 'Haiku-for-You',
     description:
-      'At the juncture of tech-enhanced creativity, this mobile app generates an original haiku for a user to edit, save and publish on a public forum.',
+      'In the quest to relieve burden of creation tech-enhanced creativity, this mobile app generates an original haiku for a user to edit, save and publish on a public forum.',
     image: '/haiku-for-you.png',
     github: 'https://github.com/cyrilmb/Haiku-For-You',
     link: 'coming soon',
@@ -34,21 +34,23 @@ const ProjectsSection = () => {
           return (
             <div key={i}>
               <div className="flex flex-col md:flex-row md:space-x-12">
-                <div className="md:w-1/2">
-                  <Image
-                    src={project.image}
-                    alt={`Screenshot of the app ${project.name}`}
-                    width={1000}
-                    height={1000}
-                    className="rounded-xl shadow-xl hover:opacity-70"
-                  />
+                <div className="mt-8 md:w-1/2">
+                  <Link href={project.link} target="_blank">
+                    <Image
+                      src={project.image}
+                      alt={`Screenshot of the app ${project.name}`}
+                      width={1000}
+                      height={1000}
+                      className="rounded-xl shadow-xl hover:opacity-70"
+                    />
+                  </Link>
                 </div>
-                <div className="md:w-1/2">
+                <div className="mt-12 md:w-1/2">
                   <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
                   <p className="text-xl leading-7 text-neutral-600 dark:text-neutral-400">
                     {project.description}
                   </p>
-                  <div className="flex flex-row align-bottom space-x-4">
+                  <div className="mt-2 flex flex-row align-bottom space-x-4">
                     <Link href={project.github} target="_blank">
                       <BsGithub
                         size={30}
