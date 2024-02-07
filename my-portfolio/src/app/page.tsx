c9b62809b5dfa6e2f9bd0feec6edde6728bdc8a4
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-02-06 23:12:32 -0600

    head element and title not working, have to cresearch why tomorrow its late

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import HeroSection from '@/components/HeroSection';
 import AboutSection from '@/components/AboutSection';
 import Testing from '@/components/Testing';
@@ -5,11 +6,16 @@ import Projects from '@/components/ProjectsSection';
 
 export default function Home() {
   return (
-    <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
-      <HeroSection />
-      <Testing />
-      <AboutSection />
-      <Projects />
-    </main>
+    <>
+      <Head>
+        <title>Cyril Malle-Barlow</title>
+      </Head>
+      <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
+        <HeroSection />
+        <Testing />
+        <AboutSection />
+        <Projects />
+      </main>
+    </>
   );
 }
--- my-portfolio/src/components/Footer.tsx
+++ my-portfolio/src/components/Footer.tsx
@@ -5,9 +5,11 @@ const Footer = () => {
   return (
     <footer className="mx-auto max-w-3x1 px-4 sm:px-6 md:max-w-5xl">
       <hr className="w-9 h-1 mx-auto my-4 bg-blue-600 border-0 rounded" />
-      <div>
-        <div>&#169; 2024 Cyril Malle-Barlow</div>
-        <div>
+      <div className="mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
+        <div className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100">
+          &#169; 2024 Cyril Malle-Barlow
+        </div>
+        <div className="flex flex-row items-center justify-center space-x-2 mb-1">
           <a
             href="https://github.com/cyrilmb/"
             rel="noreferrer"
