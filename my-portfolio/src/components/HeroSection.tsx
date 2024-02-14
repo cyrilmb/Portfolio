81861ba7a401bca7ff3935ac21a21161130b31b3
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-02-13 22:25:35 -0600

    added a spin to prof pic with a different pic on back. with dogs!

--- my-portfolio/src/components/HeroSection.tsx
+++ my-portfolio/src/components/HeroSection.tsx
@@ -9,14 +9,26 @@ const HeroSection = () => {
     <section id="home">
       <div className="flex flex-col text-center items-center justify-center mt-10 mb-5 pt-16 pb-4 md:flex-row md:space-x-6 md:text-left md:pt-12 md:pb-20 md:mt-16">
         <div className="md:w-1/2 md:-mr-0.5 md:mt-2 ">
-          <Image
-            className="rounded-full shadow-2xl md:mx-3 transition-all duration-500 [transform-style:preserve-3d] hover:[transform:rotateY(180deg)]"
-            src="/pink-door.jpeg"
-            alt="Portrait of the developer looking happy because he is standing in front of a pink door"
-            width={300}
-            height={300}
-            priority={true}
-          />
+          <div className="relative inline-block transition-all duration-500 [transform-style:preserve-3d] hover:[transform:rotateY(180deg)]">
+            <Image
+              className="rounded-full shadow-2xl md:mx-3"
+              src="/pink-door.jpeg"
+              alt="Portrait of the developer looking happy because he is standing in front of a pink door"
+              width={300}
+              height={300}
+              priority={true}
+            />
+            <div className="absolute inset-0 [transform:rotateY(180deg)] [backface-visibility:hidden]">
+              <Image
+                className="rounded-full shadow-2xl md:mx-3"
+                src="/profile-dogs.jpeg"
+                alt="Portrait of the developer looking happy because he is trying to take a selfie with his two dogs"
+                width={300}
+                height={300}
+                priority={true}
+              />
+            </div>
+          </div>
         </div>
         <div className="md:mt-2 md:w-3/5">
           <h2 className="font-bold text-2xl mt-6 md:text-5xl md:mt-0">
