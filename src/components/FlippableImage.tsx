745f39eb5b495a5d4fb2e989920f26d3ff86f0b1
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2024-04-02 20:56:43 -0500

    no img

--- src/components/FlippableImage.tsx
+++ src/components/FlippableImage.tsx
@@ -1,46 +1,47 @@
-import React, { useState } from 'react';
+// import Image from 'next/image';
+// import React, { useState } from 'react';
 
-interface FlippableImageProps {
-  frontImageSrc: string;
-  backImageSrc: string;
-}
+// interface FlippableImageProps {
+//   frontImageSrc: string;
+//   backImageSrc: string;
+// }
 
-const FlippableImage: React.FC<FlippableImageProps> = ({
-  frontImageSrc,
-  backImageSrc,
-}) => {
-  const [isFlipped, setIsFlipped] = useState(false);
+// const FlippableImage: React.FC<FlippableImageProps> = ({
+//   frontImageSrc,
+//   backImageSrc,
+// }) => {
+//   const [isFlipped, setIsFlipped] = useState(false);
 
-  const handleClick = () => {
-    setIsFlipped(!isFlipped);
-  };
+//   const handleClick = () => {
+//     setIsFlipped(!isFlipped);
+//   };
 
-  return (
-    <div className="relative w-40 h-40" onClick={handleClick}>
-      <div
-        className={`absolute w-full h-full transition-transform duration-500 transform-style-preserve-3d ${
-          isFlipped ? 'rotate-y-180' : ''
-        }`}
-      >
-        <img
-          src={frontImageSrc}
-          alt="Front Image"
-          className="w-full h-full rounded-lg shadow-lg object-cover cursor-pointer transform rotate-y-0"
-        />
-      </div>
-      <div
-        className={`absolute w-full h-full transition-transform duration-500 transform-style-preserve-3d ${
-          isFlipped ? '' : 'rotate-y-180'
-        }`}
-      >
-        <img
-          src={backImageSrc}
-          alt="Back Image"
-          className="w-full h-full rounded-lg shadow-lg object-cover cursor-pointer transform rotate-y-180"
-        />
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className="relative w-40 h-40" onClick={handleClick}>
+//       <div
+//         className={`absolute w-full h-full transition-transform duration-500 transform-style-preserve-3d ${
+//           isFlipped ? 'rotate-y-180' : ''
+//         }`}
+//       >
+//         <Image
+//           src={frontImageSrc}
+//           alt="Front Image"
+//           className="w-full h-full rounded-lg shadow-lg object-cover cursor-pointer transform rotate-y-0"
+//         />
+//       </div>
+//       <div
+//         className={`absolute w-full h-full transition-transform duration-500 transform-style-preserve-3d ${
+//           isFlipped ? '' : 'rotate-y-180'
+//         }`}
+//       >
+//         <Image
+//           src={backImageSrc}
+//           alt="Back Image"
+//           className="w-full h-full rounded-lg shadow-lg object-cover cursor-pointer transform rotate-y-180"
+//         />
+//       </div>
+//     </div>
+//   );
+// };
 
-export default FlippableImage;
+// export default FlippableImage;
